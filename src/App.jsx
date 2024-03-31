1fca570afb248f1b37fd5a847e0d7b58c72bf081
Siravat matarach <pex_der@outlook.co.th>
2024-03-31 20:19:00 +0700

    31/3/24

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { NavBar } from "./components/NavBar"
+import { Topic } from "./components/Topic"
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <NavBar />
+      <Topic />
     </>
   )
 }
